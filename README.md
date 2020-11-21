3cdb46fe918279884a3a34e9f0c67fc1bbc3c253
Howard Jones <howie@thingy.com>
2020-11-21 16:08:01 +0000

    feat initialises and draws a quadrant map

--- README.md
+++ README.md
@@ -16,25 +16,53 @@ Arrays count from 1 in basic, and 0 in Python - be careful!
 
 Everything is global
 
+The (edited!) book copy of the code includes quite a few commented-out commands
+
+There **are** typos/scanning errors in the code! (e.g. line 7570 had a goto to 1740 instead of 7740, 8590 goes to 8590 not 1590)
+
+### CONSTANTS
+
+    Ship Devices
+    1 Warp Engines
+    2 Short Range Scanner
+    3 Long Range Scanner
+    4 Phasers
+    5 Photon Tubes
+    6 Damage Control
+    7 Shield Control
+    8 Library Computer
+
 ### ARRAYS
     
     G is the galactic map. One integer for each  (100s are klingons, 10s are bases, 1s are stars)
     Z is a copy of the galactic map
     C is direction vectors for moving ship
-    D is the damage to various systems
+    D is the damage to various systems (device numbers from 1 to 8)
     
 ### VARIABLES
 
-    Q1,Q2 is your initial position
-    Z4,Z5 is your current position
+    Q$ is a 190 char string, 3 chars per position for 8*8 quadrant map (SRS)
+    Q1,Q2 is your initial position 1-8,1-8 (quadrant)
+    S1,S2 is your position *within* quadrant    
+    Z4,Z5 is your current position 1-8,1-8
     S shield level (starts at 0)
     E energy (starts at 3000)
     K9 current number of klingons
     B9 number of starbases
     T9 number of turns total
     T0 starting stardate
-    
-    
+
+    S3 stars in current quadrant
+    K3 klingons in current quadrant
+    B3 starbases in current quadrant
+    K status of up to 3 klingons in current quadrant    
+    B4,B5 coordinates of starbase (if any) in current quadrant
+        
 ### SUBROUTINES
-    
-    
+
+    6430 -     
+    8590 - find a blank spot in the local map - sets r1,r2 to location
+    8660 - insert a$ into q$ at position z1,z2 (in 190 char string) 
+    8780 - set G2$ to device name in R1
+    8830 - compare item at z1,z2 in local map with a$, set z3=1 if matching    
+    9010 - set G2$ to quadrant Z4,Z5 (without roman numerals if G5=1)
--- literal.py
+++ literal.py
@@ -2,6 +2,10 @@
 
 import math
 import random
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 def fnr(i):
@@ -14,6 +18,74 @@ def fnd(i):
     return 0
 
 
+def coords_to_strpos(z1, z2):
+    return int(z2 - 0.5) * 3 + int(z1 - 0.5) * 24
+
+
+def print_map(q_str):
+    print("---------------------------------")
+    for i in range(1, 9):
+        p = coords_to_strpos(i, 1)
+        print(q_str[p:p + 23])
+    print("---------------------------------")
+
+
+def update_local_map(mapstring, z1, z2, a_str):
+    s8 = coords_to_strpos(z1, z2)
+    logger.debug(f"Putting {a_str} at {z1},{z2} ({s8})")
+    if len(a_str) != 3:
+        raise ValueError("ERROR")
+    if s8 == 0:
+        logger.debug("Special case for first pos")
+        mapstring = a_str + mapstring[3:]
+    elif s8 == 189:
+        logger.debug("Special case for last pos")
+        mapstring = mapstring[:188] + a_str
+    else:
+        mapstring = mapstring[:s8] + a_str + mapstring[s8 + 3:]
+
+    return mapstring
+
+
+def check_map_contents(q_str, z1, z2, expected):
+    s8 = coords_to_strpos(z1, z2)
+    g = q_str[s8:s8 + 3]
+    logger.debug(f"Pos {z1},{z2} ({s8}) is '{g}' (Looking for '{expected}')")
+    if g != expected:
+        return False
+    return True
+
+
+def find_empty_map_spot(q_str):
+    while True:
+        r1 = fnr(1)
+        r2 = fnr(1)
+        if check_map_contents(q_str, r1, r2, "   "):
+            return r1, r2
+
+
+def device_name(r1):
+    names = ["WARP ENGINES", "SHORT RANGE SENSORS", "LONG RANGE SENSORS", "PHASER CONTROL", "PHOTON TUBES",
+             "DAMAGE CONTROL", "SHIELD CONTROL", "LIBRARY COMPUTER"]
+    return names[r1 - 1]
+
+
+def quadrant_name(z4, z5, g5):
+    quadrants = ["ANTARES", "RIGEL", "PROCYON", "VEGA", "CANOPUS", "ALTAIR", "SAGGITARIUS", "POLLUX", "SIRIUS", "DENEB",
+                 "CAPELLA", "BETELGEUSE", "ALDEBARAN", "REGULUS", "ARCTURUS", "SPICA"]
+    subquadrants = ["I", "II", "III", "IV"]
+    name = "BROKEN-NAME"
+    print(f"Naming {z4},{z5}")
+    if z5 <= 4:
+        name = quadrants[z4 - 1]
+    else:
+        name = quadrants[z4 + 8 - 1]
+    if g5 != 1:
+        name += " " + subquadrants[z5 % 4]
+
+    return name
+
+
 print("")
 print("                                    ,------*------,")
 print("                    ,-------------   '---  ------'")
@@ -24,8 +96,9 @@ print("")
 print("                    THE USS ENTERPRISE --- NCC-1701")
 print("")
 
-g = [[0 for x in range(8)] for y in range(8)]
-z = [[0 for x in range(8)] for y in range(8)]
+# making these 9x9 for now, to avoid 0vs1 array indices
+g = [[0 for x in range(9)] for y in range(9)]
+z = [[0 for x in range(9)] for y in range(9)]
 k = [[0 for x in range(3)] for y in range(3)]
 n = [0, 0, 0]
 d = [0, 0, 0, 0, 0, 0, 0, 0]  # initialized on 680
@@ -58,8 +131,8 @@ s2 = fnr(1)
 
 a1_str = "NAVSRSLRSPHATORSHEDAMCOMXXX"
 
-for i in range(0, 8):
-    for j in range(0, 8):
+for i in range(1, 9):
+    for j in range(1, 9):
         k3 = 0
         z[i][j] = 0
         r1 = random.random()
@@ -98,3 +171,59 @@ if b9 != 1:
 print(f"YOUR ORDERS ARE AS FOLLOWS:\n   DESTROY THE {k9} KLINGON WARSHIPS WHICH HAVE INVADED\n   THE GALAXY BEFORE "
       f"THEY CAN ATTACK FEDERATION HEADQUARTERS\n   ON STARDATE {t0 + t9}. THIS GIVES YOU {t9} DAYS. THERE{x0_str}{b9} \n"
       f"   STARBASE{x_str} IN THE GALAXY FOR RESUPPLYING YOUR SHIP\n\n")
+
+i = random.random()
+
+# on entering a new quadrant (line 1310)
+z4 = q1
+z5 = q2
+k3 = 0
+b3 = 0
+s3 = 0
+g5 = 0
+d4 = 0.5 * random.random()
+z[q1][q2] = g[q1][q2]
+
+if q1 < 1 or q1 > 8 or q2 < 1 or q2 > 8:
+    pass
+
+g2 = quadrant_name(z4, z5, g5)
+if t == t0:
+    print("YOUR MISSION BEGINS WITH YOUR STARSHIP LOCATED")
+    print(f"IN THE GALACTIC QUADRANT, '{g2}'.")
+else:
+    print(f"NOW ENTERING {g2} QUADRANT . . .")
+
+k3 = int(g[q1][q2] * 0.01)
+b3 = int(g[q1][q2] * 0.1) - 10 * k3
+s3 = g[q1][q2] - 100 * k3 - 10 * b3
+print(f"{k3} klingons, {b3} bases, {s3} stars")
+if k3 > 0:
+    print("COMBAT AREA      CONDITION RED")
+    if s <= 200:
+        print("   SHIELDS DANGEROUSLY LOW")
+for i in range(3):
+    k[i][0] = 0
+    k[i][1] = 0
+    k[i][2] = 0
+
+# q_str is a 192-char long string for the quadrant map
+q_str = 192*" "
+q_str = update_local_map(q_str, s1, s2, "<*>")
+if k3 > 0:
+    for i in range(0, k3):
+        r1, r2 = find_empty_map_spot(q_str)
+        z1 = r1
+        z2 = r2
+        q_str = update_local_map(q_str, r1, r2, "+K+")
+        k[i][0] = r1
+        k[i][1] = r2
+if b3 > 0:
+    r1, r2 = find_empty_map_spot(q_str)
+    q_str = update_local_map(q_str, r1, r2, ">!<")
+    b4 = r1
+    b5 = r2
+if s3 > 0:
+    for i in range(0, s3):
+        r1, r2 = find_empty_map_spot(q_str)
+        q_str = update_local_map(q_str, r1, r2, " * ")
